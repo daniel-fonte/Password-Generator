44a89bb76205c1936303d0113aacc8b34bf2514d
Daniel Nunes <danielnunesdafonte9@gmail.com>
2020-05-25 15:10:57 -0300

    [BACKEND]: Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,20 @@
+import cors from 'cors';
 import express from 'express';
 
 import routes from './routes';
 import server from './server';
 
 const app = server(express);
+const corsOptions = {
+  origin: '*',
+};
 
 try {
   app.middleware(express.json());
   app.middleware(express.urlencoded({
     extended: false,
   }));
+  app.middleware(cors(corsOptions));
   app.routes(routes);
   app.start(3333);
 } catch (error) {
