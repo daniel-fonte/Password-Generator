affa96c6aa5fdcd4bf3e4eda72f79b9a1f5ad655
Daniel Nunes <danielnunesdafonte9@gmail.com>
2020-05-25 11:36:08 -0300

    Add Forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,99 @@ import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Switch from './Components/Switch';
+import api from './Services/api';
 import Container from './Styles/App';
 import GlobalStyles from './Styles/GlobalStyles';
 
 function App() {
 	const [theme, setTheme] = useState({});
+	const [password, setPassword] = useState('');
+	const [lengthInput, setLengthInput] = useState(10);
+	const [uppercaseInput, setUppercaseInput] = useState(false);
+	const [numbersInput, setNumbersInput] = useState(false);
+	const [symbolsInput, setSymbolsInput] = useState(false);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		api.post('/', {
+			Length: lengthInput,
+			Uppercase: uppercaseInput,
+			Numbers: numbersInput,
+			Symbols: symbolsInput,
+		})
+			.then((res) => setPassword(res.data.password))
+			.catch(() => setPassword('Error: Try Again'));
+	}
+
 	return (
 		<>
 			<Switch alteredTheme={setTheme} />
 			<ThemeProvider theme={theme}>
 				<GlobalStyles />
-				<Container />
+				<Container>
+					<div className="passwordBox">
+						<h1>Password Generator</h1>
+						<input type="text" placeholder="Password" value={password} />
+					</div>
+					<div className="passwordOptions">
+						<form onSubmit={(event) => handleSubmit(event)} className="passwordForm">
+							<div className="option">
+								<label htmlFor="length" className="length">
+									<input
+										type="number"
+										id="length"
+										placeholder="Password Length"
+										value={lengthInput}
+										onChange={(event) => setLengthInput(event.target.value)}
+										required
+									/>
+								</label>
+							</div>
+							<div className="option">
+								<label htmlFor="uppercase" className="label-checkbox">
+									<input
+										type="radio"
+										id="uppercase"
+										name="checkbox"
+										defaultChecked={uppercaseInput}
+										onChange={(event) => setUppercaseInput(event.target.checked)}
+									/>
+									<span className="checkbox-shadow" />
+								</label>
+								<h3>Include Uppercase Letters</h3>
+							</div>
+							<div className="option">
+								<label htmlFor="numbers" className="label-checkbox">
+									<input
+										type="radio"
+										id="numbers"
+										name="checkbox"
+										defaultChecked={numbersInput}
+										onChange={(event) => setNumbersInput(event.target.checked)}
+									/>
+									<span className="checkbox-shadow" />
+
+								</label>
+								<h3>Include Numbers</h3>
+							</div>
+							<div className="option">
+								<label htmlFor="symbols" className="label-checkbox">
+									<input
+										type="radio"
+										id="symbols"
+										name="checkbox"
+										defaultChecked={symbolsInput}
+										onChange={(event) => setSymbolsInput(event.target.checked)}
+									/>
+									<span className="checkbox-shadow" />
+								</label>
+								<h3>Include Symbols</h3>
+							</div>
+							<button type="submit">Generate</button>
+						</form>
+					</div>
+				</Container>
 			</ThemeProvider>
 		</>
 	);
--- frontend/src/Styles/App.js
+++ frontend/src/Styles/App.js
@@ -2,10 +2,125 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	width: 50%;
-	margin-top: 50px;
+	margin: 50px auto;
+
+	.passwordBox {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.passwordBox h1 {
+		font-size: 30px;
+		font-weight: bold;
+	}
+
+	.passwordBox input {
+		width: 80%;
+		border: 1px solid #000;
+		border-radius: 10px;
+		padding: 10px;
+		margin-top: 10px;
+	}
+
+	.passwordOptions {
+		width: 100%;
+		margin-top: 30px;
+	}
+
+	.passwordForm {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
+		grid-row-gap: 10px;
+		grid-column-gap: 10px;
+	}
+
+	.option {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		flex-direction: row;
+	}
+
+	.option h3 {
+		text-transform: uppercase;
+		font-size: 10pt;
+	}
+
+	label.length {
+		width: 80%;
+	}
+
+	label.length input[type = number] {
+		width: 50%;
+		border: 1px solid #000;
+		border-radius: 5px;
+		padding: 5px;
+	}
+
+	label.label-checkbox {
+		cursor: pointer;
+		display: inline-block;
+		position: relative;
+		width: 30px;
+		height: 30px;
+		border-radius: 50%;
+		border: 1px solid #000;
+		margin-right: 10px;
+	}
+
+	input[type = radio] {
+		visibility: hidden;
+	}
+
+	span.checkbox-shadow {
+		display: block;
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		border-radius: 50%;
+		background-color: #000;
+		top: 4px;
+		left: 4px;
+		opacity: 0;
+		transition: all .4s;
+	}
+
+	input[type = radio]:checked + span.checkbox-shadow {
+		opacity: 1;
+	}
+
+	label.label-checkbox div {
+		width: 200px;
+		background-color: red;
+		display: flex;
+		flex-direction: row;
+		margin-left: 10px;
+	}
+
+	button[type = submit] {
+		cursor: pointer;
+		margin-top: 10px;
+		border: 1px solid #000;
+		width: 50%;
+		padding: 5px;
+		font-weight: bold;
+		font-size: 20px;
+		border-radius: 5px;
+		background-color: #a1ff00;
+	}
+
+	input[type = text], button {
+		box-shadow: 2px 2px 4px #000;
+	}
 `;
 
 export default Container;
